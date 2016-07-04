f249abeb35477a4fe0b7f0b34f8f0acea81d1df9
jfchen <jfchen@1ping.com>
2016-07-04 14:27:03 +0800

    Initial Commit

---
+++ SimeAPP/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SimeAPP
+//
+//  Created by LkSime on 16/7/4.
+//  Copyright © 2016年 LkSime. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SimeAPP/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SimeAPP
+//
+//  Created by LkSime on 16/7/4.
+//  Copyright © 2016年 LkSime. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
